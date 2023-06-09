eb923cc06d605aff7ab3f3d6a84b8a97ac08a205
ZeckLab <sturbout@gmail.com>
2023-06-09 16:30:46 +0200

    CRUD Marque

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\DataFixtures;
+use App\Entity\Marque;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -9,8 +10,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $marque = new Marque();
+        $marque->setLibelle('Renault');
+        $manager->persist($marque);
+
+        $marque2 = new Marque();
+        $marque2->setLibelle('Triumph');
+        $manager->persist($marque2);
+
+        $marque3 = new Marque();
+        $marque3->setLibelle('Harley Davidson');
+        $manager->persist($marque3);
+
 
         $manager->flush();
     }
